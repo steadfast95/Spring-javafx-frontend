db45c86715affc52af18e99b5f734b0c750113bf
steadfast95 <shker1995@gmail.com>
2019-10-06 14:06:24 +0300

    fix not update info for count employees if employees is removed

--- src/main/java/app/controller/Controller.java
+++ src/main/java/app/controller/Controller.java
@@ -98,6 +98,7 @@ public class Controller {
             restClientEmployee.deleteEmployee(employeeView.getId());
         }
         reloadEmployee();
+        reloadDepartment();
     }
 
     private void deleteDepartment() {
